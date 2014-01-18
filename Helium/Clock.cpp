da76fae4303fb7b01a8911517b8fdb7f3bafa291
b2soft <b2soft@yandex.ru>
2014-01-18 13:03:30 +0200

    build 1002

--- Helium/Clock.cpp
+++ Helium/Clock.cpp
@@ -8,8 +8,9 @@ CClock::CClock()
 
 float CClock::Update()
 {
-	m_dt = timeGetTime() - m_PrevTime;
-	m_PrevTime = timeGetTime();
+	QueryPerformanceCounter(&m_iCurrTime);
+	m_dt = (m_iCurrTime.QuadPart - m_iPrevTime.QuadPart)/m_fFreq;
+	m_iPrevTime = m_iCurrTime;
 	
 	return m_dt/divider;
 }
@@ -21,6 +22,9 @@ float CClock::DT() const
 
 void CClock::Initialize()
 {
-	m_PrevTime = timeGetTime();
+	QueryPerformanceCounter(&m_iPrevTime);
+	QueryPerformanceFrequency(&m_iFreq);
+
+	m_fFreq = (float)m_iFreq.QuadPart / 1000.0;
 }
 
--- Helium/Clock.h
+++ Helium/Clock.h
@@ -7,7 +7,11 @@ class CClock
 private:
 
 	float m_dt;
-	DWORD m_PrevTime;
+	//DWORD m_PrevTime;
+	LARGE_INTEGER m_iPrevTime;
+	LARGE_INTEGER m_iCurrTime;
+	LARGE_INTEGER m_iFreq;
+	double m_fFreq;
 
 public:
 	CClock();
